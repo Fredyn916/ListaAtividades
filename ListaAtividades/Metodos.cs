bd1a8ef81aabde3169c67ab09664b09398cf9172
Fred Marques <fredyn.curso@gmail.com>
2024-07-04 16:40:48 -0300

    Adicionar arquivos de projeto.

---
+++ ListaAtividades/Metodos.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaAtividades
+{
+    public class Metodos
+    {
+        public double EstoqueMedio { get; set; }
+
+        public void CalcularEstoqueMedio(int QuantidadeMin, int QuantidadeMax)
+        {
+            EstoqueMedio = (QuantidadeMin + QuantidadeMax) / 2;
+        }
+
+        public void DolarToReal()
+        {
+            Console.WriteLine("Digite a atual cotação do dólar:");
+            double dolar = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um valor: (dólar)");
+            double valor = int.Parse(Console.ReadLine());
+            double resultado = valor * dolar;
+
+            Console.WriteLine($"US {valor} = R$ {resultado}. DOL: {dolar}");
+        }
+    }
+}
